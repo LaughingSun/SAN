191956fd21fc44b4521af7b1f620e94b9d8c8b2d
Laughing Sun Development <erichhorn78@gmail.com>
2015-05-15 22:36:33 +0800

    test crlf crap

---
+++ src/copyright.js
@@ -0,0 +1,10 @@
+/*!
+ * SAN v<%= version %>
+ * http://laughingsun.github.io/SAN/
+ *
+ * Copyright 2014 Erich Horn
+ * Released under the MIT license
+ * http://laughingsun.github.io/SAN/license.txt
+ *
+ * Date: <%= date %>
+ */
