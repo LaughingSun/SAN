5a62ca1fc712bf1f996abc8eef57383c0a9a8462
Laughing Sun Development <erichhorn78@gmail.com>
2015-05-17 05:42:37 +0800

    alll flags conf, caching, implement test, search and match + util methods
    Make all flags configurable,
    remove main regex from instance and use caching instead
    implemented test, search and match methods.
    sdded in a few utility methods to class and prototype

--- src/gret/gret.js
+++ src/gret/gret.js
@@ -22,23 +22,32 @@
  * THE SOFTWARE.
  */
 
+/* TODOS:
+ * 
+ * y flags uses (?:.{{original}}|([^]), no y flag uses original
+ * changing y flag shoudl re-compile the regex
+ */
 var FLAG_PROP_MAP = {
-      "g": { flag:"g", index: 0, native: false, name: "global", settable: true, force: true },
-      "i": { flag:"i", index: 1, native: true , name: "ignoreCase"},
-      "m": { flag:"m", index: 2, native: true , name: "multi"},
-      "n": { flag:"n", index: 3, native: false, name: "nameOnly", settable: true },
-      "s": { flag:"s", index: 4, native: false, name: "stupid" },
-      "x": { flag:"x", index: 5, native: false, name: "extended" },
-      "y": { flag:"y", index: 6, native: false, name: "sticky", settable: true },
-      "u": { flag:"u", index: 7, native: false, name: "unicode" }
+      "g": { flag:"g", native: false, name: "global", force: true },
+      "i": { flag:"i", native: true , name: "ignoreCase"},
+      "m": { flag:"m", native: true , name: "multi"},
+      "n": { flag:"n", native: false, name: "nameOnly" },
+      "s": { flag:"s", native: false, name: "stupid" },
+      "x": { flag:"x", native: false, name: "extended" },
+      "y": { flag:"y", native: false, name: "sticky" },
+      "u": { flag:"u", native: false, name: "unicode" }
     },
-    REGEX_FLAGS   = new RegExp(["\\/([", Object.keys(FLAG_PROP_MAP).join(''), "]*)$"].join('')),
+    REGEX_FLAGS   = new RegExp(["\\/(\w*)$"].join('')),
     PREP_REGEXP  = /\\(?!k).|\((\?<(\w+)>|\?P<(\w+)>|(?!\?[:!=]))|\\k<(\w+)>|\\k'(\w+)'/g,
     CONF_LIB = 'conf/',
     CLEAN_EXT = '.clean.json',
-    REPLACER_EXT = '.replacer.json';
+    REPLACER_EXT = '.replacer.json',
+    Slice = Array.prototype.slice,
+    Max = Math.max,
+    Min = Math.min;
 
 function _checkFlags (flags, unsupported) {
+  unsupported || (unsupported = []);
   if ( flags ) {
     flags.split('').forEach(function(k){
       if ( ! (k in FLAG_PROP_MAP) )
@@ -58,127 +67,69 @@ function _getNativeFlags (flags) {
   return native.join('');
 }
 
-function _defineSettablePropFlag(flags) {
-  var _flags = [],
-      unsupported = [],
-      d, k;
-  // add flags property
-  Object.defineProperty(this, 'flags', {
-    get: function () {
-      return _flags.join('');
-    },
-    set: function (flags) {
-      var unsupported = [],
-          d, k, s;
-      for (k in FLAG_PROP_MAP) {
-        s = flags.indexOf(k) >= 0;
-        if ( (d = FLAG_PROP_MAP[f]).settable || s === _flags[k] )
-          _flags[d.index] = s ? k : undefined;
-        else
-          unsupported.push(k);
-      }
-      _checkFlags(flags, unsupported);
-    },
-    enumerable: true
-  });
-  for (k in FLAG_PROP_MAP) {
-    // initialize property value
-    _flags[(d = FLAG_PROP_MAP[k]).index] = (flags.indexOf(k) >= 0) ? k : undefined;
-    // add property
-    if ( d.settable )
-      (function ( flag, name, index ) {
-        Object.defineProperty(this, name, {
-          get: function () {
-            return !! _flags[index];
-          },
-          set: function (state) {
-            _flags[index] = state ? flag : undefined;
-          },
-          enumerable: true
-        });
-      }).call(this, k, d.name, d.index);
-    else
-      Object.defineProperty(this, d.name, {
-        value: !! _flags[d.index],
-        enumerable: true
-      });
+function _method_prep ( input, startIndex, endIndex ) {
+  var regex;
+  regex = this.native;
+  if ( typeof input === 'string' && this.input !== input )
+    this.input = input;
+  if ( startIndex != null )
+    this.startIndex = startIndex;
+  if ( endIndex != null )
+    this.endIndex = endIndex;
+  if ( ! this.global )
+    this.lastIndex = this.startIndex;
+  regex.lastIndex = Max(this.startIndex, this.lastIndex);
+  return regex
+//  console.log( '_method_prep', regex,  regex.lastIndex, this.input, this.startIndex, this.endIndex, this.lastIndex );
+}
+
+function _getNamedCaptures ( matched, namedIndexes ) {
+  var named = {},
+      i, k;
+  if ( matched && namedIndexes ) {
+    for (k in namedIndexes)
+      if ( matched[i = namedIndexes[k]] !== undefined )
+        named[k] = matched[i];
   }
-  _checkFlags(flags, unsupported);
+  return named;
+}
+
+function __exec__ ( native ) {
+  var startIndex = this.startIndex,
+      endIndex = this.endIndex,
+      matched;
+  if ( (matched = native.exec(this.input))
+      && matched.index >= startIndex
+      && matched.index < endIndex
+      && ( ! this.sticky || matched.pop() === undefined) ) {
+    matched.named = _getNamedCaptures( matched, native.namedIndexes );
+  } else {
+    matched = null;
+  }
+  this.lastIndex = native.lastIndex;
+  return matched;
 }
 
 /**
- * parse source (RegExp.source) with named captures / references, returns a new source and a named index map
- * 
- * @function _createRegexExecNamedSupport
- * @argument {string}   source - either a RegExp instance or a RegExp.source value
- * @argument {Object?}  namedIndexes - a namedIndexes indexes map
- * @returns {RegExp} a native RegExp.source, and sets namedIndexes indexes
+ * match array
  */
-var _sourceCache = {},
-    _namedIndexesCache = {};
-function _compileSource(source, flags, namedIndexes) {
-  var sk = ['/', source, '/', flags].join(''),
-      chain,
-      ci, pi, m, t;
-  if ( sk in _sourceCache ) {
-    m = _namedIndexesCache[sk];
-    for (t in m)
-      namedIndexes[t] = m[t];
-    return _sourceCache[sk];
-  }
-  chain = [];
-  ci = 1;
-  PREP_REGEXP.lastIndex = pi = 0;
-  while ( (m = PREP_REGEXP.exec(source)) ) {
-//    console.log( source, pi, m );
-    chain.push(source.slice(pi,m.index));  // add preceeding part
-    pi = PREP_REGEXP.lastIndex;
-    if ( (t = m[2]) ) {         // namedIndexes capture
-      namedIndexes[t] = ci;
-      chain.push('(');
-      ci++;
-    } else if ( (t = m[4] || m[5] || m[6]) ) {  // namedIndexes backreference
-      if ( ! namedIndexes[t] )
-        throw new Error(['no such namedIndexes back reference "', t, '"'].join(''));
-      chain.push('\\', namedIndexes[t]);
-    } else {
-      chain.push(m[0]);
-    }
-  }
-  chain.push(source.slice(pi));  // add trailing part
-  m = {};
-  for (t in namedIndexes)
-    m[t] = namedIndexes[t];
-  _namedIndexesCache[sk] = m;
-  return _sourceCache[sk] = chain.join('');
-}
-
-function _method_prep ( input, index, endIndex ) {
-  if ( typeof input === 'string' )
-    this.input = input;
-  if ( index === true || this.global )
-    this.index = this.lastIndex;
-  else
-    this.index = index || 0;
-  if ( endIndex == null )
-    this.endIndex = input.length;
-  return this.input.slice(this.index, this.endIndex);
+function _exec ( input, startIndex, endIndex ) {
+  return __exec__(_method_prep.call(this, input, startIndex, endIndex));
 }
 
 /**
  * true or false
  */
-function _test ( input, index, endIndex ) {
-  var part = _method_prep.call(this, input, index, endIndex), 
-      matched = this.native.test(true ? input : part);
-  return matched;
+function _test ( input, startIndex, endIndex ) {
+  return !! __exec__(_method_prep.call(this, input, startIndex, endIndex));
 }
 
 /**
  * index or -1
  */
-function _search ( input, index, endIndex ) {
-  throw new Error('not yet supported, but it will be within days, check back soon')
+function _search ( input, startIndex, endIndex ) {
+  var matched = __exec__(_method_prep.call(this, input, startIndex, endIndex));
+  return matched ? matched.index : -1;
 }
 
 /**
@@ -188,19 +139,18 @@ function _search ( input, index, endIndex ) {
  *    ["the whole string", "whole", index: 4, lastIndex: 9],
  *    ["the whole string", "string", index: 10, lastIndex: 16] ]
  */
-function _match ( input, index, endIndex ) {
-  throw new Error('not yet supported, but it will be within days, check back soon')
-}
-
-/**
- * match array
- */
-function _exec ( input, index, endIndex ) {
-  var part = _method_prep.call(this, input, index, endIndex), 
-      matched = this.native.exec(true ? input : part);
-  if ( matched )
-    this.named = matched
-  return matched;
+function _match ( input, startIndex, endIndex ) {
+  var native = _method_prep.call(this, input, startIndex, endIndex),
+      matched = [],
+      g = this.global,
+      m;
+  do {
+    if ( (m = __exec__(native)) )
+      matched.push( m );
+  } while( g && m );
+  if ( matched.length )
+    return g ? matched[0] : matched;
+  return null;
 }
 
 /**
@@ -233,51 +183,147 @@ function _loadCleaner ( name, options ) {
   }
 }
 
+/**
+ * parse source (RegExp.source) with named captures / references, returns a new source and a named index map
+ * 
+ * @function _compileSource
+ * @argument {string}   source - either a RegExp instance or a RegExp.source value
+ * @argument {Object?}  namedIndexes - a namedIndexes indexes map
+ * @returns {RegExp} a native RegExp.source, and sets namedIndexes indexes
+ */
+var _regexCache = {};
+function _compileSource(source, flags) {
+  var key,
+      regex, sticky, chain, namedIndexes,
+      ci, pi, m, t;
+  if ( source instanceof RegExp ) {
+    if ( flags == null )
+      flags = REGEX_FLAGS.exec(source.toString())[1];
+    source = source.flags;
+  } else if ( ! flags )
+    flags = '';
+  this.dirty = false;
+  if ( (key = ['/', source, '/', flags].join('')) in _regexCache )
+    return this.native = _regexCache[key];
+  
+  chain = (sticky = (flags.indexOf('y') >= 0)) ? ['(?:'] : [];
+  namedIndexes = {};
+  ci = 1;
+  PREP_REGEXP.lastIndex = pi = 0;
+  while ( (m = PREP_REGEXP.exec(source)) ) {
+    chain.push(source.slice(pi,m.index));  // add preceeding part
+    pi = PREP_REGEXP.lastIndex;
+    if ( (t = m[2]) ) {         // namedIndexes capture
+      namedIndexes[t] = ci;
+      chain.push('(');
+      ci++;
+    } else if ( (t = m[4] || m[5] || m[6]) ) {  // namedIndexes backreference
+      if ( ! namedIndexes[t] )
+        throw new Error(['no such namedIndexes back reference "', t, '"'].join(''));
+      chain.push('\\', namedIndexes[t]);
+    } else {
+      chain.push(m[0]);
+    }
+  }
+  sticky    // add trailing part
+      ? chain.push(source.slice(pi), ')|([^])')
+      : chain.push(source.slice(pi) );
+  (regex = new RegExp(chain.join(''), _getNativeFlags(flags).replace('g','') + 'g')).namedIndexes = namedIndexes;
+  return (this.native = _regexCache[key] = regex);
+}
+
+function _defineSettablePropFlag() {
+  var _flags = Array(Object.keys(FLAG_PROP_MAP).length),
+      i, k;
+  // add flags property
+  Object.defineProperty(this, 'flags', {
+    get: function () {
+      return _flags.join('');
+    },
+    set: function (flags) {
+      var i = -1,
+          k, s;
+      flags || (flags = '');
+      for (k in FLAG_PROP_MAP) {
+        if ( (s = (flags.indexOf(k) >= 0) ? k : undefined) !== _flags[++i] ) {
+          _flags[i] = s;
+          this.dirty = true;
+        }
+      }
+      _checkFlags(flags);
+    },
+    enumerable: true
+  });
+  i = -1;
+  for (k in FLAG_PROP_MAP) {
+    // add property
+    (function ( flag, name, index ) {
+      Object.defineProperty(this, name, {
+        get: function () {
+          return !! _flags[index];
+        },
+        set: function (state) {
+          _flags[index] = state ? flag : undefined;
+          this.dirty = true;
+        },
+        enumerable: true
+      });
+    }).call(this, k, FLAG_PROP_MAP[k].name, ++i);
+  }
+}
+
 function Gret (source, flags) {
   var _source,
-      _named,
-      _namedIndexes,
-      _regex,
+      _native,
       _input;
   if ( ! (this instanceof Gret) )
     throw new Error('constructor called as function');
-  if ( _source instanceof RegExp ) {
-    source = source.source;
-    (flags == null) && (flags = REGEX_FLAGS.exec(source.toString()));
-  }
-  _defineSettablePropFlag.call(this, flags || '');
-//  console.log('flags' , flags, this.flags);
-//  console.log('_getNativeFlags(this.flags)' , _getNativeFlags(this.flags));
-  _source = _compileSource(source, this.flags, _namedIndexes = {});
-  _regex = new RegExp(_source, _getNativeFlags(this.flags));
+  _defineSettablePropFlag.call(this);
   Object.defineProperties(this, {
-    native: { value: _regex },
-    source: { value: source, enumerable: true },
+    source: {
+      get: function () {
+        return _source
+      },
+      set: function ( source ) {
+        _source = source instanceof RegExp ? source.source : source;
+        this.dirty = true;
+      },
+      enumerable: true
+    },
+    native: {
+      get: function () {
+        if ( this.dirty )
+          _native = this.compile( this.source, this.flags );
+        return _native;
+      },
+      set: function ( regex ) {
+        _native = regex;
+        this.dirty = false;
+      },
+      enumerable: true
+    },
     input: {
       get: function () {
         return _input
       },
       set: function ( text ) {
-        this.lastIndex = this.index = 0;
+        this.startIndex = 0;
         this.endIndex = (_input = text).length;
+        this.index = -1;
       },
       enumerable: true
     },
-    named: {
-      get: function () {
-        return _named;
-      },
-      set: function (matches) {
-        _named = {};
-        for (k in _namedIndexes)
-          if ( matches[i = _namedIndexes[k]] !== undefined )
-            _named[k] = matches[i];
-        matches.named = _named;
-      },
+    compile: {
+      value: _compileSource,
       enumerable: true
     }
   });
-  this.input = '';
+  this.flags    = (typeof flags === 'string')
+    ? flags
+    : ((source instanceof RegExp && REGEX_FLAGS.exec(source.toString())[1]) || '');
+  this.source   = source;
+  this.input    = '';
+  this.lastIndex= 0;
 }
 
 Gret.prototype = Object.create(RegExp.prototype, {
@@ -299,15 +345,10 @@ Object.defineProperties(Gret, {
         flags.push(k);
       return flags.join('');
   })() },
-  SETTABLEFLAGS: { value: (function(){
-      var flags = [],
-          k;
-      for ( k in FLAG_PROP_MAP )
-        if ( FLAG_PROP_MAP[k].settable )
-          flags.push(k);
-      return flags.join('');
-  })() },
-  loadCleaner:  { value: _loadCleaner }     // string (this does a predefined replace with options
+  loadCleaner:      { value: _loadCleaner },     // string (this does a predefined replace with options
+  nativeFlags:      { value: _getNativeFlags },
+  validateFlags:    { value: _checkFlags },
+  getNamedCaptures: { value: _getNamedCaptures }
 });
 
 
--- test/gret.test.js
+++ test/gret.test.js
@@ -1,33 +1,75 @@
 Gret = require('../src/gret/gret.js');
 
-function test ( g ) {
-  console.log(g);
-  console.log('Is g an instance of Gret? ' + (g instanceof Gret)); // true
-  console.log('Is g an instance of RegExp? ' + (g instanceof RegExp)); // true
-  console.log('g.source? '+g.source);
-  console.log('g.flags? '+g.flags);
-  console.log('g.native? '+g.native);
-  console.log('g.input? '+g.input);
-  console.log('g.index? '+g.index);
-  console.log('g.endIndex? '+g.endIndex);
-  console.log('g.lastIndex? '+g.lastIndex);
-  console.log('g.toString()? '+g.toString());
-  console.log('g.test(r.toString())? '+g.test(g.toString()));
+function stringify ( val, indent ) {
+  return JSON.stringify(val, function (err, obj){
+    if ( obj instanceof RegExp && ! (obj instanceof Gret) )
+      return obj.toString();
+    return obj;
+  }, indent);
 }
 
-var r = /^\/((?:[^\\\/]+|\\.)*)\/(\w*)$/,
-    v;
-console.log(Gret);
-console.log('Gret.SETTABLEFLAGS', Gret.SETTABLEFLAGS);
-console.log('Gret.SUPPORTEDFLAGS', Gret.SUPPORTEDFLAGS);
+function test ( source, flags, input, startIndex, endIndex ) {
+  var regex = new Gret(source, flags),
+      g = regex.global;
+  console.log('regex = new Gret(', stringify(source), ', ', stringify(flags), ')');
+  console.log('Is regex an instance of Gret? ' + (regex instanceof Gret)); // true
+  console.log('Is regex an instance of RegExp? ' + (regex instanceof RegExp)); // true
+  console.log('regex? ' + stringify(regex, 2));
+  if ( input ) {
+    console.log('input? ' + input);
+    console.log('startIndex? ' + startIndex);
+    console.log('endIndex? ' + endIndex);
+    do {
+      console.log(['regex.exec(', input, ', ', startIndex, ', ', endIndex, ')'].join(''), m = regex.exec( input, startIndex, endIndex ));
+    } while( g && m );
+  }
+}
 
-test( r );
-test( new Gret(r.source) );
-test( new Gret(r.source, 'y') );
-test( new Gret(r.source, 'g') );
-test( new Gret(r.source, Gret.SETTABLEFLAGS) );
-test( new Gret(r.source, Gret.SUPPORTEDFLAGS) );
-test( r = new Gret('(?<word>\\w+)', Gret.SUPPORTEDFLAGS) );
-do {
-  console.log( 'v = r.exec("Every day I write the book.")', v = r.exec("Every day I write the book.") );
-} while (v);
+var s = '(?<word>\\w+)',
+    t = "every day I write the book.",
+    start = 5,
+    end = t.length - start;
+//console.log(Gret);
+//console.log('Gret.SUPPORTEDFLAGS', Gret.SUPPORTEDFLAGS);
+//console.log('Gret.getNativeFlags(', Gret.SUPPORTEDFLAGS, ')', Gret.nativeFlags(Gret.SUPPORTEDFLAGS));
+
+test( s, undefined, t, start, end );
+test( s, '', t, start, end );
+test( s, 'g', t, start, end );
+test( s, 'y', t, start, end );
+test( s, Gret.nativeFlags(Gret.SUPPORTEDFLAGS), t, start, end );
+test( s, Gret.SUPPORTEDFLAGS, t, start, end );
+
+//e = '(?<word>\\w+)';
+//t = "every day I write the book.";
+//i = 10;
+//
+//r = new Gret(e);
+//console.log( 'r', JSON.stringify(r, null, 2) );
+//do {
+//  console.log( 'v = r.exec("' + t + '",', i, ')', v = r.exec(t, i) );
+//} while (v && r.global);
+//
+//r = new Gret(e, 'g');
+//console.log( 'r', JSON.stringify(r, null, 2) );
+//t = "every day I write the book.";
+//i = 10;
+//do {
+//  console.log( 'v = r.exec("' + t + '",', i, ')', v = r.exec(t, i) );
+//} while (v && r.global);
+//
+//r = new Gret(e, 'y');
+//console.log( 'r', JSON.stringify(r, null, 2) );
+//t = "every day I write the book.";
+//i = 10;
+//do {
+//  console.log( 'v = r.exec("' + t + '",', i, ')', v = r.exec(t, i) );
+//} while (v && r.global);
+//
+//r = new Gret(e, Gret.SUPPORTEDFLAGS);
+//console.log( 'r', JSON.stringify(r, null, 2) );
+//t = "every day I write the book.";
+//i = 10;
+//do {
+//  console.log( 'v = r.exec("' + t + '",', i, ')', v = r.exec(t, i) );
+//} while (v && r.global);
